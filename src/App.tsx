c8072207c68c0670dcbb12e01be43ea01805fa1e
Guilherme Vianna <59426923+Guilherme-Vianna@users.noreply.github.com>
2025-03-21 12:24:26 -0300

    feat: teste

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export default function App() {
   const [quantity, setQuantity] = useState<number>(1);
   const [availableOptions, setAvailableOptions] = useState([
     {
-      value: 'teste',
+      value: 'cheeseburger',
       label: 'Cheeseburger',
       removable: ['Alface', 'Tomate', 'Cebola', 'Picles', 'Maionese'],
       addable: [
