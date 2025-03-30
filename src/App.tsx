bf7e1160b62833f2890a2ccc0f8106f37bba73c2
AlehandroSV <alehandrosantos99@gmail.com>
2025-03-30 14:38:33 -0300

    feat: blocked reload when pulling tab up

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router';
 import HomePage from './pages/home';
 import OrderPage from './pages/order';
@@ -7,6 +8,22 @@ import ProductsPage from './pages/products';
 import ReportsPage from './pages/reports';
 
 export default function App() {
+  useEffect(() => {
+    const preventPullToRefresh = (event: TouchEvent) => {
+      if (window.scrollY === 0) {
+        event.preventDefault();
+      }
+    };
+
+    document.addEventListener('touchmove', preventPullToRefresh, {
+      passive: false,
+    });
+
+    return () => {
+      document.removeEventListener('touchmove', preventPullToRefresh);
+    };
+  }, []);
+
   return (
     <Routes>
       <Route index element={<HomePage />} />
