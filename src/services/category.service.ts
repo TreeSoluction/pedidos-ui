1d1300acea43fc82074ee7606c498e1741f7865c
Guilherme Vianna <59426923+Guilherme-Vianna@users.noreply.github.com>
2025-04-01 16:48:13 -0300

    fix: rotas atualizadas

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -1,13 +1,13 @@
 import api from './index';
 
 export const GetAllCategories = async () => {
-  const request = await api.get('category');
+  const request = await api.get('categories');
 
   return request.data;
 };
 
 export const GetCategoryById = async (id: string) => {
-  const request = await api.get(`category/${id}`);
+  const request = await api.get(`categories/${id}`);
 
   return request.data;
 };
--- src/services/plots.service.ts
+++ src/services/plots.service.ts
@@ -1,28 +1,30 @@
 import api from './index';
 
 export const GetTrendingProductsWeeksOld = async () => {
-  const request = await api.get('/plot/weeksold/trending-products');
+  const request = await api.get('/plots/weeksold/trending-products');
   return request.data;
 };
 
 export const GetProductDaySold = async (productId: string) => {
-  const request = await api.get(`/plot/daysold/product?productId=${productId}`);
+  const request = await api.get(
+    `/plots/daysold/product?productId=${productId}`,
+  );
   return request.data;
 };
 
 export const GetProductWeekSold = async (productId: string) => {
   const request = await api.get(
-    `/plot/weeksold/product?productId=${productId}`,
+    `/plots/weeksold/product?productId=${productId}`,
   );
   return request.data;
 };
 
 export const GetWeekSold = async () => {
-  const request = await api.get('/plot/weeksold');
+  const request = await api.get('/plots/weeksold');
   return request.data;
 };
 
 export const GetDiarySold = async () => {
-  const request = await api.get('/plot/diarysold');
+  const request = await api.get('/plots/diarysold');
   return request.data;
 };
